e326bc4fbc66da4045a190f81d304c5cc6bb1980
Sophie Shin <sophie@alloy.co>
2021-02-16 22:47:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ function add2(n) {
   // Feel free to move things around!
 }
 
-var funkyFunction = function theFunk() {
-  return function theFunk() {
+var funkyFunction = function () {
+  return function () {
     return "FUNKY!"
   }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction()
+theFunk = funkyFunction()
